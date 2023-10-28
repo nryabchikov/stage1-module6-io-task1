c97a417f7fce7fb142945b958a3453c4425a2ea4
Nekitos4 <ryabchikovnik241104@gmail.com>
2023-10-28 15:53:13 +0300

    feat: io(FileReader)

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,62 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        java.io.FileReader stream = null;
+        try {
+            stream = new java.io.FileReader(file);
+        } catch (IOException e) {
+            System.out.println("Can't open stream.");
+        }
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            int elem;
+            try {
+                elem = stream.read();
+                if (elem == -1)
+                    break;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            sb.append((char) elem);
+        }
+        try {
+            stream.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String[] array = getStringsFromFile(sb);
+        String name = array[0];
+        int age = Integer.parseInt(array[1]);
+        String email = array[2];
+        long phone = Long.parseLong(array[3]);
+        return new Profile(name, age, email, phone);
+    }
+    private String[] getStringsFromFile(StringBuilder sb) {
+        int count = 0;
+        int counter = 0;
+        String[] array = new String[4];
+        while (count < sb.length()) {
+            char elem = sb.charAt(count);
+            if (elem == ':') {
+                count++;
+                StringBuilder data = new StringBuilder();
+                while (elem != '\n') {
+                    count++;
+                    elem = sb.charAt(count);
+                    data.append(elem);
+                }
+                data.delete(data.length() - 1, data.length());
+                array[counter] = data.toString();
+                counter++;
+            }
+            count++;
+        }
+        return array;
     }
 }
